52ff40635f16b9b413bac6be183bcc98a81102a7
floiz <floiz777xxx@gmail.com>
2025-01-23 15:19:53 +1000

    implementation of 4 labs

--- src/pages/Project/Projects.tsx
+++ src/pages/Project/Projects.tsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
-import { Project } from "../../types/Project";
-import { ModalProject } from "./ModalProject";
-import { AddModalProject } from "./AddModalProject";
-import { RootState, AppDispatch } from "../../store";
-import { useSelector, useDispatch } from "react-redux";
-import { removeProject } from "../../store/projectsSlice";
-import { FaTrash } from "react-icons/fa";
+import {useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState, AppDispatch} from "../../store";
+import {fetchAndAddGitHubProjects} from "../../services/githubService";
+import {Project} from "../../types/Project";
+import {ModalProject} from "./ModalProject";
+import {AddModalProject} from "./AddModalProject";
+import {removeProject} from "../../store/projectsSlice";
+import {FaTrash} from "react-icons/fa";
+import {Button, Spinner, Pagination} from "flowbite-react";
+
+const ITEMS_PER_PAGE = 5;
 
 export const Projects = () => {
     const dispatch = useDispatch<AppDispatch>();
@@ -13,14 +17,31 @@ export const Projects = () => {
     const [selectedTech, setSelectedTech] = useState<string>("All");
     const [selectedProject, setSelectedProject] = useState<Project | null>(null);
     const [isAddModalOpen, setIsAddModalOpen] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [currentPage, setCurrentPage] = useState<number>(1);
 
-    const technologies = Array.from(
-        new Set(projects.flatMap((project) => project.technologies))
+    // Функция для загрузки проектов с задержкой
+    const loadProjects = async () => {
+        setLoading(true);
+        setTimeout(async () => {
+            await dispatch((dispatch, getState) => fetchAndAddGitHubProjects("WARINMYSOUL", dispatch, getState));
+            setLoading(false);
+        }, 1000);
+    };
+
+    useEffect(() => {
+        loadProjects();
+    }, [dispatch]);
+
+    const technologies = Array.from(new Set(projects.flatMap((project) => project.technologies || [])));
+    const filteredProjects = projects.filter(project =>
+        selectedTech === "All" ? true : project.technologies?.includes(selectedTech)
     );
 
-    const filteredProjects = projects.filter((project) =>
-        selectedTech === "All" ? true : project.technologies.includes(selectedTech)
-    );
+    // Логика для отображения текущих проектов на странице
+    const totalPages = Math.ceil(filteredProjects.length / ITEMS_PER_PAGE);
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+    const currentProjects = filteredProjects.slice(startIndex, startIndex + ITEMS_PER_PAGE);
 
     const openModal = (project: Project) => setSelectedProject(project);
     const closeModal = () => setSelectedProject(null);
@@ -29,24 +50,54 @@ export const Projects = () => {
         dispatch(removeProject(id));
     };
 
+    const handleTechChange = (tech: string) => {
+        setSelectedTech(tech);
+        setCurrentPage(1);
+    };
+
+    const onPageChange = (page: number) => setCurrentPage(page); // Обработчик изменения страницы
+
     return (
-        <div className="container font-sans text-gray-800">
+        <div className="container mx-auto font-sans text-gray-800">
             <h2 className="text-4xl text-black mb-5 text-center">Проекты</h2>
 
+            {/* Кнопка для обновления и добавления проектов */}
+            <div className="flex justify-center mb-5 space-x-4">
+                <Button
+                    onClick={loadProjects}
+                    className="bg-blue-500 text-white px-6 py-1 rounded-lg shadow-md hover:bg-blue-600 transition-transform transform hover:-translate-y-1"
+                >
+                    {loading ? (
+                        <>
+                            <Spinner size="sm" aria-label="Загрузка проектов..."/>
+                            <span className="pl-3">Загрузка...</span>
+                        </>
+                    ) : (
+                        "Обновить проекты"
+                    )}
+                </Button>
+                <Button
+                    onClick={() => setIsAddModalOpen(true)}
+                    className="bg-green-500 text-white px-6 py-1 rounded-lg shadow-md hover:bg-green-600 transition-transform transform hover:-translate-y-1"
+                >
+                    Добавить проект
+                </Button>
+            </div>
+
             {projects.length > 0 && (
-                <div className="container flex justify-center flex-wrap mb-5 px-4 space-x-4">
+                <div className="container flex justify-center flex-wrap mb-5 px-4 space-x-4 ">
                     <button
-                        onClick={() => setSelectedTech("All")}
+                        onClick={() => handleTechChange("All")}
                         className={`px-6 py-3 shadow-md transition-transform transform hover:-translate-y-1 m-2 ${
                             selectedTech === "All" ? "bg-blue-500 text-white" : "bg-white text-gray-700"
                         } border border-gray-300 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-lg`}
                     >
                         Все
                     </button>
-                    {technologies.map((tech) => (
+                    {technologies.map((tech, index) => (
                         <button
-                            key={tech}
-                            onClick={() => setSelectedTech(tech)}
+                            key={tech || index}
+                            onClick={() => handleTechChange(tech)}
                             className={`px-6 py-3 shadow-md transition-transform transform hover:-translate-y-1 m-2 ${
                                 selectedTech === tech ? "bg-blue-500 text-white" : "bg-white text-gray-700"
                             } border border-gray-300 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-lg`}
@@ -57,30 +108,37 @@ export const Projects = () => {
                 </div>
             )}
 
-            <ul className="container list-none p-0 max-w-3xl mx-auto">
-                {filteredProjects.map((project, index) => (
+            <ul className="container list-none p-0 max-w-3xl mx-auto ">
+                {currentProjects.map((project, index) => (
                     <li
                         key={index}
-                        className="relative bg-white p-5 mb-4 rounded-lg shadow-md transition-transform transform hover:-translate-y-1 cursor-pointer"
+                        onClick={() => openModal(project)}
+                        className="relative bg-white p-5 mb-4 rounded-lg shadow-md transition-all transform hover:scale-105 hover:bg-gray-200 cursor-pointer"
                     >
                         <div className="flex justify-between items-start">
-                            <h3 className="text-2xl mb-2" onClick={() => openModal(project)}>
+                            <h3 className="text-2xl mb-2 text-blue-800">
                                 {project.title}
                             </h3>
                             <FaTrash
-                                onClick={() => handleDeleteProject(project.id)}
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    handleDeleteProject(project.id);
+                                }}
                                 className="text-red-500 cursor-pointer hover:text-red-700"
                                 title="Удалить проект"
                             />
                         </div>
                         <p className="text-lg leading-relaxed mb-2">{project.description}</p>
                         <div className="mb-3 text-gray-600">
-                            <span className="text-sm font-semibold">Технологии: {project.technologies.join(", ")}</span>
+        <span className="text-sm font-semibold">
+            Технологии: {Array.isArray(project.technologies) ? project.technologies.join(", ") : "Нет данных"}
+        </span>
                         </div>
                         <a
                             href={project.link}
                             target="_blank"
                             rel="noopener noreferrer"
+                            onClick={(e) => e.stopPropagation()}
                             className="text-blue-400 font-bold hover:underline"
                         >
                             Посмотреть проект
@@ -89,18 +147,19 @@ export const Projects = () => {
                 ))}
             </ul>
 
-            <ModalProject project={selectedProject} onClose={closeModal} />
 
-            <div className="flex justify-center mt-10 mb-10">
-                <button
-                    onClick={() => setIsAddModalOpen(true)}
-                    className="bg-green-500 text-white px-6 py-3 rounded-lg shadow-md hover:bg-green-600 transition-transform transform hover:-translate-y-1"
-                >
-                    Добавить проект
-                </button>
+            {/* Пагинация */}
+            <div className="flex justify-center mt-6 mb-6">
+                <Pagination
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    onPageChange={onPageChange}
+                    showIcons
+                />
             </div>
 
-            <AddModalProject isOpen={isAddModalOpen} onClose={() => setIsAddModalOpen(false)} />
+            <ModalProject project={selectedProject} onClose={closeModal}/>
+            <AddModalProject isOpen={isAddModalOpen} onClose={() => setIsAddModalOpen(false)}/>
         </div>
     );
 };
